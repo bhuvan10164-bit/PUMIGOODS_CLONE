b8bfd81c2bcac53ff0c8e20d300841acb0bace86
Your Name <bhuvan10164@gmail.com>
2026-03-23 19:25:57 +0530

    api

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://axalin-project.onrender.com/api' });
+const api = axios.create({ baseURL: 'https://pumigoods-clone.onrender.com/api' });
 
 export const fetchProducts = () => api.get('/products/');
 export const fetchProduct = (id: number) => api.get(`/products/${id}/`);
